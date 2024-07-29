9d6b5dcaa4ac3be2ede543c8a74a1bd369e02b4f
Roshma <roshanmorris@msn.com>
2024-07-29 19:19:15 +0530

    updated errors in Resources.tsx and Hero.tsx

--- src/app/(pages)/Resources/page.tsx
+++ src/app/(pages)/Resources/page.tsx
@@ -4,21 +4,20 @@ import React from 'react'
 const page = () => {
   return (
     <div className='container'>
-    <div className="row">
-     <div className="col">
-     <div className="col-lg-11">
-     <p className="lead">Diploma in Cloud Computing and ERP </p>
-     <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Resources </h1>
-    <p className='lead'>Github repository <span> <Link href='https://github.com/Roshma/CBTSystem1.0.git'>'https://github.com/Roshma/CBTSystem1.0.git'</Link></span></p>
-      <br></br>
-      <p className='lead'>Login to AWS <span> <Link href='https://aws.amazon.com/'>Amazon Web Services (AWS)</Link></span></p>
-      <br></br>
-      <p className='lead'>Login to Azure <span> <Link href='https://azure.microsoft.com/en-us'>Mocrosoft Azure </Link></span></p>
-   </div>
-
-     </div>
+      <div className="row">
+        <div className="col">
+          <div className="col-lg-11">
+            <p className="lead">Diploma in Cloud Computing and ERP</p>
+            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Resources</h1>
+            <p className='lead'>Github repository <span> <Link href='https://github.com/Roshma/CBTSystem1.0.git'>&apos;https://github.com/Roshma/CBTSystem1.0.git&apos;</Link></span></p>
+            <br />
+            <p className='lead'>Login to AWS <Link href='https://aws.amazon.com/'>Amazon Web Services (AWS)</Link></p>
+            <br />
+            <p className='lead'>Login to Azure <Link href='https://azure.microsoft.com/en-us'>Microsoft Azure</Link></p>
+          </div>
+        </div>
+      </div>
     </div>
- </div>
   )
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,26 @@
 'use client'
 
 import React from 'react'
+import Image from 'next/image'
 import "../app/globals.css"
-
 
 const Hero = () => {
   return (
     <div className='herodev'>
-    <div className="container col-xxl-8 px-4 py-5">
-    <div className="row flex-lg-row-reverse align-items-center g-3 py-1">
-      <div className="col-10 col-sm-8 col-lg-6">
-        <img src={`/HeroCyberSec1.jpg`} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
-      </div>
-      <div className="col-lg-6">
-        <p className="lead">This website is created as the final assignment of </p>
-        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Diploma in Cloud Computing and ERP</h1>
-        <p className="lead">The website contains client and server components.   On front-end it has 4 pages, namely, ‘Home’, ‘Contact’, ‘About’ and 'Resources'.  All four pages have different routes.</p>
-        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-        
-
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-3 py-1">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <Image src={`/HeroCyberSec1.jpg`} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width={700} height={500} loading="lazy" />
+          </div>
+          <div className="col-lg-6">
+            <p className="lead">This website is created as the final assignment of</p>
+            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Diploma in Cloud Computing and ERP</h1>
+            <p className="lead">The website contains client and server components. On front-end it has 4 pages, namely, &lsquo;Home&rsquo;, &lsquo;Contact&rsquo;, &lsquo;About&rsquo; and &lsquo;Resources&rsquo;. All four pages have different routes.</p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start"></div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  </div>
   )
 }
 
